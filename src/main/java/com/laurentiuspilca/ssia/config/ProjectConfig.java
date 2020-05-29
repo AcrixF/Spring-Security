d9e8e29c9ebf01114f642bdb95020d0c0f720b3c
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-05-29 18:09:30 -0500

    Override AuthenticationManagerBuilder

--- src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -1,45 +1,34 @@
 package com.laurentiuspilca.ssia.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests()
                 .anyRequest()
-                .authenticated();
+                .permitAll();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailService = new InMemoryUserDetailsManager();
 
-        var user = User.withUsername("Neoa")
-                .password("12345")
-                .authorities("read")
-                .build();
+        var user = User.withUsername("Neoa").password("12345").authorities("read").build();
 
-        userDetailsService.createUser(user);
+        userDetailService.createUser(user);
 
-        return userDetailsService;
+        auth.userDetailsService(userDetailService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-
 }
