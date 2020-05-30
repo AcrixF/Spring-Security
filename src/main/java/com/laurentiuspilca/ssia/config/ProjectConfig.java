fb598cba664956529b226736af9dfc6c973d1029
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-05-30 13:24:26 -0500

    Custom Authentication Provider

--- src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -1,15 +1,22 @@
 package com.laurentiuspilca.ssia.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(customAuthenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,15 +24,5 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .anyRequest()
                 .permitAll();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Neoa")
-                .password("123456789")
-                .authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
