8adc5df099fb5a600889cab54e2056d309af5ad5
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-06-09 19:56:52 -0500

    Customizer  -> Custom Entry Point

--- src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +19,20 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AuthenticationProvider authenticationProvider;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Customizer
+        http.httpBasic(customizer -> {
+            customizer.realmName("Spring Customizer");
+            customizer.authenticationEntryPoint(new CustomEntryPoint());
+        });
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
