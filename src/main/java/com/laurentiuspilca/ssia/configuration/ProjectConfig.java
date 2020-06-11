4785064e5b8cf77bf775b68ef8b5ff1d30239566
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-06-11 16:28:27 -0500

    Deny by Roles

--- src/main/java/com/laurentiuspilca/ssia/configuration/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/configuration/ProjectConfig.java
@@ -18,13 +18,11 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        String expression = "hasAuthority('READ') and !hasAuthority('DELETE')";
-
         http.httpBasic();
 
         http.authorizeRequests()
                 .anyRequest()
-                .access(expression);
+                .hasRole("ADMIN");
     }
 
     @Bean
@@ -34,12 +32,12 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
         UserDetails user_1 = User.withUsername("Neoa")
                 .password("12345")
-                .authorities("READ")
+                .roles("ADMIN")
                 .build();
 
         UserDetails user_2 = User.withUsername("AcrixF")
                 .password("12345")
-                .authorities("READ","WRITE", "DELETE")
+                .roles("MANAGER")
                 .build();
 
         userDetailsManager.createUser(user_1);
