70723bcfa8da94ea9e142f35a9be004cbb05ffdb
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-06-11 12:10:58 -0500

    Simple Authentication Web Application

---
+++ src/main/java/com/laurentiuspilca/ssia/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.laurentiuspilca.ssia.repository;
+
+import com.laurentiuspilca.ssia.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByUsername(String username);
+}
---
+++ src/main/java/com/laurentiuspilca/ssia/services/ProductService.java
@@ -0,0 +1,20 @@
+package com.laurentiuspilca.ssia.services;
+
+import com.laurentiuspilca.ssia.entities.Product;
+import com.laurentiuspilca.ssia.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+}
