0c565010922138abbf55b15df4d6998132842799
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-06-16 13:56:49 -0500

    MVCMatcher PathVariables && Regex Expressions

--- src/main/java/com/laurentiuspilca/ssia/configuration/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/configuration/ProjectConfig.java
@@ -2,7 +2,6 @@ package com.laurentiuspilca.ssia.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -22,10 +21,10 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic();
 
         http.authorizeRequests()
-                .mvcMatchers("/a/b/**")
-                    .authenticated()
+                .mvcMatchers("/product/{code:^[0-9]*$}")
+                    .permitAll()
                 .anyRequest()
-                    .permitAll();
+                    .denyAll();
 
         http.csrf().disable();
 
